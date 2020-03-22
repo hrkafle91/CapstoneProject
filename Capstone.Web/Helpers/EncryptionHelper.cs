09cf3127d545cbbf587a2714bdbba35fcb454ce3
Hemraj Kafle <kafleh@sheridancollege.ca>
2020-03-22 02:16:33 -0400

    Added EncryptText method in EncryptionHelper class

--- Capstone.Web/Helpers/EncryptionHelper.cs
+++ Capstone.Web/Helpers/EncryptionHelper.cs
@@ -10,5 +10,21 @@ namespace Capstone.Web.Helpers
     /// </summary>
     public class EncryptionHelper
     {
+        public static string EncryptText(string plainText)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            StringBuilder str = new StringBuilder();
+            UnicodeEncoding unicode = new UnicodeEncoding();
+
+            byte[] hash = md5.ComputeHash(unicode.GetBytes(plainText));
+
+            for (int i = 0; i < 100; i++)
+                hash = md5.ComputeHash(hash);
+
+            for (int i = 0; i < hash.Length; i++)
+                str.Append(hash[i].ToString("X2"));
+
+            return str.ToString();
+        }
     }
 }
