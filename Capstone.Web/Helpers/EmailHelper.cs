06f571c115e23e7ccb322de03ecd595857368cc0
Hemraj Kafle <kafleh@sheridancollege.ca>
2020-03-21 23:35:13 -0400

    Created helper classes that will be used by all projects for common actions

---
+++ Capstone.Web/Helpers/EmailHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Helpers
+{
+    public class EmailHelper
+    {
+        public static void SendEmail(string email, string subject, string message)
+        {
+
+        }
+    }
+}
