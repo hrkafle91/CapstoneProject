cbb30505fea1b9fd3cbcccf789a23acfd37ad816
Hemraj Kafle <kafleh@sheridancollege.ca>
2020-04-05 16:02:23 -0400

    Login functionality added to the application.

--- Capstone.Web/Controllers/LoginAccountController.cs
+++ Capstone.Web/Controllers/LoginAccountController.cs
@@ -2,16 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using DBModel;
 using System.Web.Mvc;
+using Capstone.Web.Helpers;
+using Capstone.Web.ViewModels;
+using DBModel.Repositories;
 
 namespace Capstone.Web.Controllers
 {
     public class LoginAccountController : Controller
     {
-        // GET: LoginAccount
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult VerifyAccount(string emailId, string password)
+        {
+            using (AccountRepository repo = new AccountRepository())
+            {
+                Account account = repo.GetAllAccounts().Where(x => x.emailID == emailId).FirstOrDefault();
+                if ((account != null) && (account.password == password))
+                {
+                    return Json(true);
+                }
+                else
+                {
+                    return Json(false);
+                }
+            }
         }
     }
 }
---
+++ Capstone.Web/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string emailID { get; set; }
+        public string password { get; set; }
+    }
+}
