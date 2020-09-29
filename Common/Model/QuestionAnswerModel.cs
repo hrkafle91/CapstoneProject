d08bca60fb2f68a3591177130750acf664d16455
Hemraj Kafle <kafleh@sheridancollege.ca>
2020-09-28 21:01:35 -0400

    Added a model named 'QuestionAnswerModel'

---
+++ Common/Model/QuestionAnswerModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Model
+{
+    public class QuestionAnswerModel
+    {
+        public int QuestionId { get; set; }
+        public bool? IsNone { get; set; } = false;
+        public bool? IsBeginner { get; set; } = false;
+        public bool? IsIntermediate { get; set; } = false;
+    }
+}
