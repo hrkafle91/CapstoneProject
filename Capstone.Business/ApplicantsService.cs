0b97430c85381530b74125d5576f9b3d4daa26a7
Elizabeth Smikle <smiklee@sheridancollege.ca>
2020-11-08 14:04:40 -0500

    Added SearchApplicants and Leaderboard functionality and styling

---
+++ Capstone.Business/ApplicantsService.cs
@@ -0,0 +1,42 @@
+﻿using Common.Enumerations;
+using Common.Model;
+using DBModel;
+using DBModel.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Business
+{
+    public class ApplicantsService
+    {
+        private static AccountRepository Repository = new AccountRepository();
+
+        public static List<UserViewModel> GetAllApplicants()
+        {
+            var accounts = Repository.GetAllAccounts();
+            List<UserViewModel> applicants = new List<UserViewModel>();
+
+            foreach(Account acc in accounts)
+            {
+                if(acc.userType.Equals(UserTypeEnum.Applicant.ToString()))
+                {
+                    UserViewModel applicant = UserService.SetUser(acc);
+                    applicant.CompletionPercentage = BadgesService.GetCompletionPercentage(acc.userID);
+                    applicants.Add(applicant);
+                }
+            }
+
+            return applicants;
+        }
+
+        public static UserViewModel GetApplicant(int id)
+        {
+            var account = Repository.GetAccount(id);
+
+            return UserService.SetUser(account);
+        }
+    }
+}
--- Capstone.Web/Controllers/AdminController.cs
+++ Capstone.Web/Controllers/AdminController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Capstone.Business;
+using Common.Model;
+using Common.Enumerations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +14,78 @@ namespace Capstone.Web.Controllers
         // GET: Admin
         public ActionResult Index()
         {
-            return View();
+            var applicants = ApplicantsService.GetAllApplicants();
+
+            return View(applicants);
+        }
+
+        public PartialViewResult SearchApplicants(string searchText, char val)
+        {
+            List<UserViewModel> model = ApplicantsService.GetAllApplicants();
+            searchText = searchText.ToLower();
+            List<UserViewModel> result = null;
+
+            switch (val)
+            {
+                case '1':
+                    result = model.Where(x => x.FirstName.ToLower().Contains(searchText)).ToList();
+                    break;
+                case '2':
+                    result = model.Where(x => x.Lastname.ToLower().Contains(searchText)).ToList();
+                    break;
+                case '3':
+                    result = model.Where(x => x.CompletionPercentage.ToString().Contains(searchText)).ToList();
+                    break;
+                case '4':
+                    result = model.Where(x => x.EmailId.ToLower().Contains(searchText)).ToList();
+                    break;
+            }
+
+            return PartialView("_TableView", result);
+        }
+
+        public PartialViewResult FilterApplicantsByCareerPathThenSort(char filterVal, char sortVal)
+        {
+            List<UserViewModel> model = ApplicantsService.GetAllApplicants();
+            List<UserViewModel> result = null;
+
+            LogService.Write("FilterVal: " + filterVal);
+            if (filterVal.Equals('1'))
+            {
+                result = model.Where(x => x.CareerPath.Equals(CareerPath.Administrator.ToString()) && (x.CompletionPercentage > 0.0) ).ToList();
+                LogService.Write("Admin:" + result.Count.ToString());
+            }
+            else if (filterVal.Equals('2'))
+            {
+                result = model.Where(x => x.CareerPath.Equals(CareerPath.Developer.ToString()) && x.CompletionPercentage > 0.0).ToList();
+            }
+            else
+            {
+                result = model.Where(x => x.CompletionPercentage > 0.0).ToList();
+                LogService.Write("Default:" + result.Count.ToString());
+            }
+
+            switch (sortVal)
+            {
+                case '1':
+                    result = result.OrderBy(x => x.FirstName).ToList();
+                    break;
+                case '2':
+                    result = result.OrderBy(x => x.Lastname).ToList();
+                    break;
+                case '3':
+                    result = result.OrderBy(x => x.CareerPath).ToList();
+                    break;
+                case '5':
+                    result = result.OrderBy(x => x.EmailId).ToList();
+                    break;
+                default:
+                    result = result.OrderByDescending(x => x.CompletionPercentage).ToList();
+                    break;
+            }
+
+
+            return PartialView("_TableView", result);
         }
 
         public ActionResult SideMenu()
